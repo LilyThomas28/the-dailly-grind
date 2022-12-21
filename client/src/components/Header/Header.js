b8a8a98146c5113f90ed53359f8271b650f843a0
David Luu <dluu.audeo@gmail.com>
2022-12-20 22:43:30 -0800

    setup

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,7 +5,7 @@ import "./header.css";
 export default function Header() { 
     return (
         <div className="header">
-            <h1>Help for Hire</h1>
+            <h1>Daily Grinds</h1>
             <NavLink to="/">Home</NavLink> | <NavLink to="/chat">Chat</NavLink> | <NavLink to="/contact">Contact</NavLink> | <NavLink to="/login">Login</NavLink> | <NavLink to="/profile">Profile</NavLink> | <NavLink to="/signup">Signup</NavLink>
         </div>
     );
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,7 +1,24 @@
 import React from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+
+import './home.css'
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Home() {
   return (
-    <h1 className='text-center'>Home Page</h1>
+    <div className="d-flex justify-content-center">
+      <Card style={{ width: "50%" }} className="text-center p-4 m-4">
+        <Card.Body>
+          <Card.Title><h1 className="title">Home Page</h1></Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">
+            Click the button to find a coffee shop near you!
+          </Card.Subtitle>
+          <Card.Link href="Contact">
+            <Button>Card Link</Button>
+            </Card.Link>
+        </Card.Body>
+      </Card>
+    </div>
   );
 }
